190faf4a48940baa0d9a6984174f2f3b5efc2938
Diana <U23221294@utp.edu.pe>
2026-05-13 10:45:53 -0500

    feat: implementar factorial iterativo

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+// V1: Implementación iterativa
+public class Factorial {
+
+    public static long calcular(int n) {
+        long resultado = 1;
+
+        for (int i = 1; i <= n; i++) {
+            resultado = resultado * i;
+        }
+
+        return resultado;
+    }
+}
